e7dbfc0fe5ab25ff6ec4fb9a5a5da3629065badf
saulocapistrano <saulocapistrano@gmail.com>
2024-11-11 15:25:40 -0300

    feat: configurar rotas para login e dashboard

---
+++ dac-front/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
