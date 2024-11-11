7851388eb0cce34c27475318cc371403d2ee595b
saulocapistrano <saulocapistrano@gmail.com>
2024-11-11 15:26:51 -0300

    feat: implementar formulário de login e lógica de autenticação

--- dac-front/src/app/auth/login/login.component.ts
+++ dac-front/src/app/auth/login/login.component.ts
@@ -1,12 +1,27 @@
+// src/app/auth/login/login.component.ts
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  credentials = { email: '', password: '' };
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.authService.login(this.credentials).subscribe({
+      next: (response) => {
+        console.log('Login realizado com sucesso:', response);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        console.error('Erro ao fazer login:', error);
+      }
+    });
+  }
 }
